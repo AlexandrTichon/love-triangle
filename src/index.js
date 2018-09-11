837fc94e4dab22dfafb60e35ea1e5b2602e88b61
AlexandrTichon <38797690+AlexandrTichon@users.noreply.github.com>
2018-09-12 00:11:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var TreangleNumber = 0;                      //numbers of triangles
+  var TriangleMass = [preferences.length];      //array of element usage
+  for (let i = 0; i < TriangleMass.length; i++)
+  {
+      TriangleMass[i] = true;         //all elements can used
+  }
+  for ( i = 0; i < preferences.length ; i++)
+  {                                                                     //check all parametrs
+    if (preferences[preferences[preferences[i] - 1] - 1] === (i+1) && TriangleMassCheck(i, TriangleMass) && CheckMoving(preferences, i))
+    {
+        TriangleMass[i] = TriangleMass[[preferences[i] - 1]] = TriangleMass[preferences[preferences[i] - 1] - 1] = false; //register used elements
+        TreangleNumber++;
+    }
+  }
+  return TreangleNumber;
+
+
+  function CheckMoving(CheckingArray = [], index)   //people can't love himself (it's a joke)
+  {
+      if (CheckingArray[index] == CheckingArray[CheckingArray[index] - 1])return false;
+      else return true;
+  }
+
+
+  function TriangleMassCheck(index, CheckingMass = []) //check elements for using
+  {
+      if (CheckingMass[index] == false)return false;          //return false if if use and true if not
+      else return true;
+  }
 };
